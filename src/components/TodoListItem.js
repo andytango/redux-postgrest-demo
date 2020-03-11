35ca03b198e4fa409c11240ccc7f52d5efcd442b
Andrew Hall <andrew@rioja.io>
2020-03-11 18:43:59 +0000

    Extract sub components

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,6 +1,6 @@
-import React from "react";
-import TodoEditForm from './TodoEditForm'
-import TodoDeleteButton from './TodoDeleteButton'
+import React, { useCallback } from "react";
+import TodoDeleteButton from "./TodoDeleteButton";
+import TodoEditForm from "./TodoEditForm";
 
 export default function TodoListItem({
   todo_id,
@@ -12,27 +12,42 @@ export default function TodoListItem({
   todo_idx
 }) {
   const { editState, setEditState } = editTodo;
+  const onContentClick = useCallback(() => setEditState({ todo_id, content }), [
+    setEditState,
+    todo_id,
+    content
+  ]);
+  const isEditing = editState.todo_id === todo_id;
+  const onImageClick = useCallback(() => {
+    setImageState({ todo_idx, show: true });
+  }, [todo_idx, setImageState]);
+
   return (
     <div className="todo">
       <TodoDeleteButton {...{ todo_id }} />
       <strong>
-        {editState.todo_id === todo_id ? (
+        {isEditing ? (
           <TodoEditForm {...{ todo_id, editTodo }} />
         ) : (
-          <span onClick={() => setEditState({ todo_id, content })}>
-            {content}
-          </span>
+          <span onClick={onContentClick}>{content}</span>
         )}
       </strong>
-      {content_image && (
-        <img
-          className="todo-image"
-          alt="uploaded with the todo"
-          src={`data:*/*;base64,${content_image}`}
-          onClick={() => setImageState({ todo_idx, show: true })}
-        />
-      )}
-      <span className="todo-date">{new Date(created_at).toLocaleString()}</span>
+      {content_image && <TodoImage onClick={onImageClick} />}
+      <TodoDate date={created_at} />
     </div>
   );
+}
+
+function TodoImage({ content_image, ...props }) {
+  return (
+    <img
+      className="todo-image"
+      alt="uploaded with the todo"
+      src={`data:*/*;base64,${content_image}`}
+    />
+  );
+}
+
+function TodoDate({date}) {
+  return <span className="todo-date">{new Date(date).toLocaleString()}</span>;
 }
