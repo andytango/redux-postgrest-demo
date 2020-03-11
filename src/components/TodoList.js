e9991c89a91feddf730df0311ce29229980ab654
Andrew Hall <andrew@rioja.io>
2020-03-11 18:07:45 +0000

    Extract TodoListItem component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,17 @@
-import React, { useState, useCallback } from "react";
-import {
-  useGetTodos,
-  useDeleteTodos,
-  usePatchTodos,
-  useEditTodos
-} from "../helpers/hooks";
+import React, { useState } from "react";
+import { useEditTodos, useGetTodos } from "../helpers/hooks";
+import TodoListItem from "./TodoListItem";
 
 export default function Todos() {
   const todos = useGetTodos();
   const [imageState, setImageState] = useState({ show: false });
   const { editRow, editState } = useEditTodos();
+  const listItemProps = {
+    editRow,
+    editState,
+    imageState,
+    setImageState
+  };
 
   if (todos && imageState.show) {
     return (
@@ -27,60 +29,12 @@ export default function Todos() {
   return (
     <div className="todos">
       {todos &&
-        todos.map(
-          ({ todo_id, content, content_image, created_at }, todo_idx) => (
-            <div className="todo" key={todo_id}>
-              <TodoDeleteButton {...{ todo_id }} />
-              <strong>
-                {editState.todo_id === todo_id ? (
-                  <TodoEditForm {...{ todo_id, editRow, editState }} />
-                ) : (
-                  <span onClick={() => editRow(todo_id, content)}>
-                    {content}
-                  </span>
-                )}
-              </strong>
-              {content_image && (
-                <img
-                  className="todo-image"
-                  alt="uploaded with the todo"
-                  src={`data:*/*;base64,${content_image}`}
-                  onClick={() => setImageState({ todo_idx, show: true })}
-                />
-              )}
-              <span className="todo-date">
-                {new Date(created_at).toLocaleString()}
-              </span>
-            </div>
-          )
-        )}
+        todos.map((props, todo_idx) => (
+          <TodoListItem
+            {...{ ...props, ...listItemProps, todo_idx }}
+            key={todo_idx}
+          />
+        ))}
     </div>
-  );
-}
-
-function TodoDeleteButton({ todo_id }) {
-  const dispatchDeleteAction = useDeleteTodos();
-  return <button onClick={() => dispatchDeleteAction(todo_id)}>╳</button>;
-}
-
-function TodoEditForm({ todo_id, editRow, editState: { content } }) {
-  const dispatchPatchAction = usePatchTodos();
-  const handleButtonClick = useCallback(
-    payload => {
-      editRow({});
-      dispatchPatchAction(todo_id, payload);
-    },
-    [editRow, dispatchPatchAction, todo_id]
-  );
-
-  return (
-    <span>
-      <input
-        type="text"
-        onChange={e => editRow(todo_id, e.target.value)}
-        value={content}
-      />
-      <button onClick={() => handleButtonClick(content)}>Done</button>
-    </span>
   );
 }
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,62 @@
+import React, { useCallback } from "react";
+import { useDeleteTodos, usePatchTodos } from "../helpers/hooks";
+
+export default function TodoListItem({
+  todo_id,
+  content,
+  content_image,
+  created_at,
+  editRow,
+  editState,
+  setImageState,
+  todo_idx
+}) {
+  return (
+    <div className="todo">
+      <TodoDeleteButton {...{ todo_id }} />
+      <strong>
+        {editState.todo_id === todo_id ? (
+          <TodoEditForm {...{ todo_id, editRow, editState }} />
+        ) : (
+          <span onClick={() => editRow(todo_id, content)}>{content}</span>
+        )}
+      </strong>
+      {content_image && (
+        <img
+          className="todo-image"
+          alt="uploaded with the todo"
+          src={`data:*/*;base64,${content_image}`}
+          onClick={() => setImageState({ todo_idx, show: true })}
+        />
+      )}
+      <span className="todo-date">{new Date(created_at).toLocaleString()}</span>
+    </div>
+  );
+}
+
+function TodoDeleteButton({ todo_id }) {
+  const dispatchDeleteAction = useDeleteTodos();
+  return <button onClick={() => dispatchDeleteAction(todo_id)}>╳</button>;
+}
+
+function TodoEditForm({ todo_id, editRow, editState: { content } }) {
+  const dispatchPatchAction = usePatchTodos();
+  const handleButtonClick = useCallback(
+    payload => {
+      editRow({});
+      dispatchPatchAction(todo_id, payload);
+    },
+    [editRow, dispatchPatchAction, todo_id]
+  );
+
+  return (
+    <span>
+      <input
+        type="text"
+        onChange={e => editRow(todo_id, e.target.value)}
+        value={content}
+      />
+      <button onClick={() => handleButtonClick(content)}>Done</button>
+    </span>
+  );
+}
